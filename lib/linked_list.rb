fca3faaafc7d55bebf1ebf00d5ce59db34c6ba8d
Ariel Katz <captainkatz@gmail.com>
2015-11-30 22:22:01 -0500

    make first set of tests for LinkedList#[] pass

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -2,5 +2,23 @@ require 'pry'
 require_relative 'node'
 
 class LinkedList
- 
+  attr_accessor :first_node
+
+  def initialize
+    @first_node = nil
+  end
+
+  def prepend(info)
+    foo = Node.new(info)
+    foo.next_node = self.first_node
+    self.first_node = foo
+  end
+
+  def [](index)
+    current_node = self.first_node
+    index.times do
+      current_node = current_node.next_node
+    end
+    current_node
+  end
 end
--- lib/node.rb
+++ lib/node.rb
@@ -1,2 +1,8 @@
 class Node
+  attr_accessor :next_node, :info
+
+  def initialize(info)
+    @info = info
+    @next_node = nil
+  end
 end
